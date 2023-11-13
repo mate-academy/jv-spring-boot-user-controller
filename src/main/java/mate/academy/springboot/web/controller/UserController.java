2095e78f6dfbd09d3c61975db013613694a6f322
Volodymyr <volodymyrdavidov1@gmail.com>
2023-11-13 14:41:50 +0200

    use @RequestMapping

--- src/main/java/mate/academy/springboot/web/controller/UserController.java
+++ src/main/java/mate/academy/springboot/web/controller/UserController.java
@@ -2,20 +2,18 @@ package mate.academy.springboot.web.controller;
 
 import java.util.List;
 import mate.academy.springboot.web.model.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
-    @GetMapping("/users")
+    @GetMapping
     public List<User> getListOfUsers() {
         return List.of(new User(1L, "bob@i.ua"), new User(2L, "alice@i.ua"));
     }
 
-    @PostMapping("/users")
+    @PostMapping
     public String createdUser(@RequestBody User user) {
         return "User created. Id: %s, email: %s".formatted(user.id(),user.email());
     }
