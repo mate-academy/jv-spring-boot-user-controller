8f654a27051c75c42722debec5680d5a81f94629
Stipaxa <petrivs572@gmail.com>
2023-11-23 14:36:48 +0200

    Solution

--- src/main/java/mate/academy/springboot/web/controller/UserController.java
+++ src/main/java/mate/academy/springboot/web/controller/UserController.java
@@ -1,6 +1,5 @@
 package mate.academy.springboot.web.controller;
 
-import java.util.Collections;
 import java.util.List;
 import mate.academy.springboot.web.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
