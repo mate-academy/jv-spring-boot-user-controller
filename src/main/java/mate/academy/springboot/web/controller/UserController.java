9733a6f221c1c755cbc82b1d931ea084d0277305
Nikita <kaptetos@gmail.com>
2023-12-17 12:53:24 +0200

    fulfilled according to the conditions

--- src/main/java/mate/academy/springboot/web/controller/UserController.java
+++ src/main/java/mate/academy/springboot/web/controller/UserController.java
@@ -1,5 +1,26 @@
 package mate.academy.springboot.web.controller;
 
+import java.util.List;
+import mate.academy.springboot.web.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
+    // GET: /users
+    @GetMapping
+    public List<User> getUsers() {
+        return List.of(new User(1L, "bob@i.ua"), new User(2L, "alice@i.ua"));
+    }
+
+    // POST: /users
+    @PostMapping
+    public String createUser(@RequestBody User user) {
+        return String.format("User created. Id: %s, email: %s", user.id(), user.email());
+    }
 }
