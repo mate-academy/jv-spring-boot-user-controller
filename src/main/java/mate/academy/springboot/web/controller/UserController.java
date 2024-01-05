a325d67eae3bd2e664d7d6527cf39defd7f448a5
Nikita <nsalogub96@gmail.com>
2024-01-05 17:46:09 +0200

    fix issues

--- src/main/java/mate/academy/springboot/web/controller/UserController.java
+++ src/main/java/mate/academy/springboot/web/controller/UserController.java
@@ -1,21 +1,24 @@
 package mate.academy.springboot.web.controller;
 
-import mate.academy.springboot.web.model.User;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+import mate.academy.springboot.web.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
     @PostMapping
-    public User save(@RequestBody User user) {
-        return new User(user.id(), user.email());
+    public String save(@RequestBody User user) {
+        return "User created. Id: %s, email: %s".formatted(user.id(), user.email());
     }
 
     @GetMapping
     public List<User> getAllUsers() {
-        return List.of(new User(1L, "bob@gmail.com"), new User(2L, "alice@gmail.com"));
+        return List.of(new User(1L, "bob@i.ua"), new User(2L, "alice@i.ua"));
     }
 }
