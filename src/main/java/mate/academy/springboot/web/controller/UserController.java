349023152f462d339f7f28ef083d8035ced225a5
arrem4eg <arem4eg@gmail.com>
2024-01-08 14:17:17 +0100

    fixed spring-boot-user-controller

--- src/main/java/mate/academy/springboot/web/controller/UserController.java
+++ src/main/java/mate/academy/springboot/web/controller/UserController.java
@@ -1,12 +1,12 @@
 package mate.academy.springboot.web.controller;
 
+import java.util.List;
 import mate.academy.springboot.web.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
-import java.util.List;
 
 @RestController
 @RequestMapping("/users")
