7feaadcc62a6baa536dba82ed3d919c9b474c07c
Olga Starkova <ole4kastar79@gmail.com>
2024-02-26 00:29:02 +0200

    created and tested first web application! Starikova

--- src/main/java/mate/academy/springboot/web/controller/UserController.java
+++ src/main/java/mate/academy/springboot/web/controller/UserController.java
@@ -1,14 +1,12 @@
 package mate.academy.springboot.web.controller;
 
+import java.util.List;
 import mate.academy.springboot.web.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -20,6 +18,6 @@ public class UserController {
 
     @PostMapping
     public String save(@RequestBody User user) {
-        return "User created. Id: %s, email: %s".formatted(user.id(),user.email());
+        return "User created. Id: %s, email: %s".formatted(user.id(), user.email());
     }
 }
