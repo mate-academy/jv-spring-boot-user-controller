df200933c4fa10b1c4a9286ef5234632bd65d4a2
Pavlo Polezhay <pasha200417@gmail.com>
2024-02-28 10:30:21 +0200

    little fixes

--- src/main/java/mate/academy/springboot/web/controller/UserController.java
+++ src/main/java/mate/academy/springboot/web/controller/UserController.java
@@ -1,8 +1,8 @@
 package mate.academy.springboot.web.controller;
 
 import mate.academy.springboot.web.model.User;
-import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
