70c20fc0d4dbc60d5abbf4ae18e0be45baca1e76
Yevhen Nedashkivskyi <kesh040593@ukr.net>
2024-04-02 18:33:58 +0300

    hw-spring-boot-controller

--- src/main/java/mate/academy/springboot/web/controller/UserController.java
+++ src/main/java/mate/academy/springboot/web/controller/UserController.java
@@ -1,5 +1,20 @@
 package mate.academy.springboot.web.controller;
 
+import java.util.List;
+import mate.academy.springboot.web.model.User;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
+    @GetMapping
+    public List<User> getAllUsers() {
+        return List.of(new User(1L, "bob@i.ua"), new User(2L, "alice@i.ua"));
+    }
+
+    @PostMapping
+    public String saveUser(@RequestBody User user) {
+        return "User created. Id: %s, email: %s".formatted(user.id(), user.email());
+    }
 }
