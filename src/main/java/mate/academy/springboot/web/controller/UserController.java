ca35404950280eb3a034730560b685fc1fa07757
bobik <oliferchukrivne@i.ua>
2024-04-19 19:29:54 +0300

    done2

--- src/main/java/mate/academy/springboot/web/controller/UserController.java
+++ src/main/java/mate/academy/springboot/web/controller/UserController.java
@@ -1,13 +1,11 @@
 package mate.academy.springboot.web.controller;
 
+import java.util.List;
 import mate.academy.springboot.web.model.User;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class UserController {
