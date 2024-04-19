4dd2996195aed1b0bf5314b71e714347358d8d31
Nazar Klimovych <rksvjdbx@gmail.com>
2024-04-19 22:56:23 +0300

    Added implementation for User controller

--- src/main/java/mate/academy/springboot/web/controller/UserController.java
+++ src/main/java/mate/academy/springboot/web/controller/UserController.java
@@ -1,5 +1,29 @@
 package mate.academy.springboot.web.controller;
 
-public class UserController {
+import java.util.List;
+import mate.academy.springboot.web.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
+@RequestMapping("/users")
+public class UserController {
+    @GetMapping
+    @ResponseBody
+    public List<User> getAll() {
+        return List.of(
+                new User(1L, "alice@gmail.com"),
+                new User(2L, "bob@gmail.com"));
+    }
+
+    @PostMapping
+    @ResponseBody
+    public String addUser(@RequestBody User user) {
+        return String.format("User successfully created to DB. "
+                + "Id: %s, email: %s", user.id(), user.email());
+    }
 }
