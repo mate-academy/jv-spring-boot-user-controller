795d05be7ea2c50a66c8e4490ff9f14faae473ae
Oleksandr Samoylenko <ntskazhu1985@gmail.com>
2024-04-22 16:31:20 +0300

    Solution was added

--- src/main/java/mate/academy/springboot/web/controller/UserController.java
+++ src/main/java/mate/academy/springboot/web/controller/UserController.java
@@ -1,5 +1,20 @@
 package mate.academy.springboot.web.controller;
 
+import mate.academy.springboot.web.model.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
+    public List<User> getAll() {
+        return List.of(new User(1L, "bob@i.ua"),
+                        new User(2L, "alice@i.ua"));
+    }
+
+    public String save(User user) {
+        return "User created. Id: %s, email: %s".formatted(user.id(), user.email());
+    }
 }
