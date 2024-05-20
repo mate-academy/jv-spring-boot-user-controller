9586b885898145d767f0dca0313582a7f9ecb5c3
Oleh <popovich158@gmail.com>
2024-05-20 18:43:44 +0300

    Implemented UserController class

--- src/main/java/mate/academy/springboot/web/controller/UserController.java
+++ src/main/java/mate/academy/springboot/web/controller/UserController.java
@@ -1,5 +1,25 @@
 package mate.academy.springboot.web.controller;
 
+import java.util.List;
+import mate.academy.springboot.web.model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
+    @GetMapping
+    public List<User> getUsers() {
+        return List.of(
+                new User(1L, "bob@i.ua"),
+                new User(2L, "alice@i.ua")
+        );
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createUser(@RequestBody User user) {
+        String response = String.format("User created. Id: %s, email: %s", user.id(), user.email());
+        return ResponseEntity.ok(response);
+    }
 }
