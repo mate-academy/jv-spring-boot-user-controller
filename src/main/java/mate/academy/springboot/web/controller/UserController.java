fa729554aef746d14b6ae18c66a5f2f9f13e11cc
Oleh <popovich158@gmail.com>
2024-05-20 18:45:40 +0300

    formatted code

--- src/main/java/mate/academy/springboot/web/controller/UserController.java
+++ src/main/java/mate/academy/springboot/web/controller/UserController.java
@@ -3,7 +3,11 @@ package mate.academy.springboot.web.controller;
 import java.util.List;
 import mate.academy.springboot.web.model.User;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
