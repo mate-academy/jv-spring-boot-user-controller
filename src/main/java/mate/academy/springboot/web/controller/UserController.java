28f3d1cf303894b2a593946091f8f781663ede22
Your NameMykola <mykola.osolinskyi@gmail.com>
2024-06-23 21:27:48 +0300

    build success.new branch

--- src/main/java/mate/academy/springboot/web/controller/UserController.java
+++ src/main/java/mate/academy/springboot/web/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
     @GetMapping
     public List<User> getAll() {
         return List.of(new User(1L, "bob@i.ua"), new User(2L, "alice@i.ua"));
