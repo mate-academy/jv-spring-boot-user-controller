b7d25f43b77a7e72a476b4007b02caf76c3f5aed
citruscaviar <citruscaviar96@gmail.com>
2024-09-07 12:33:21 +0300

    completed the task 2

--- src/main/java/mate/academy/springboot/web/controller/UserController.java
+++ src/main/java/mate/academy/springboot/web/controller/UserController.java
@@ -1,7 +1,6 @@
 package mate.academy.springboot.web.controller;
 
 import java.util.List;
-
 import mate.academy.springboot.web.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
