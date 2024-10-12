cdfbda6a76cb92e9acd8463f374ec1fa4c5ecad7
Anton Pavliuk <streetreadger@gmail.com>
2024-10-12 22:42:14 +0300

    Fix style error

--- src/main/java/mate/academy/springboot/web/controller/UserController.java
+++ src/main/java/mate/academy/springboot/web/controller/UserController.java
@@ -1,5 +1,6 @@
 package mate.academy.springboot.web.controller;
 
+import java.util.List;
 import mate.academy.springboot.web.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,13 +8,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/users")
 public class UserController {
     @GetMapping
-    public List<User> getUsers(){
+    public List<User> getUsers() {
         return List.of(
                 new User(1L, "bob@i.ua"),
                 new User(2L, "alice@i.ua")
