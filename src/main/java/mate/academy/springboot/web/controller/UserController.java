aa068bc558bec14bb92bb1b34b12a2cd31004c3a
margosha99 <grete9597@gmail.com>
2025-01-10 13:39:47 +0200

    issue fixed

--- src/main/java/mate/academy/springboot/web/controller/UserController.java
+++ src/main/java/mate/academy/springboot/web/controller/UserController.java
@@ -17,6 +17,6 @@ public class UserController {
 
     @PostMapping("/users")
     public String save(@RequestBody User user) {
-        return "User created: Id: %s, email: %s".formatted(user.id(), user.email());
+        return "User created. Id: %s, email: %s".formatted(user.id(), user.email());
     }
 }
