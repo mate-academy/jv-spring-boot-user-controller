5c3b1ea4f4da99aea7c6163f480e5607375cc7d9
smagles <lesialist@icloud.com>
2025-01-26 16:56:35 +0100

    fix checkstyle errors

--- src/main/java/mate/academy/springboot/web/controller/UserController.java
+++ src/main/java/mate/academy/springboot/web/controller/UserController.java
@@ -1,12 +1,11 @@
 package mate.academy.springboot.web.controller;
 
+import java.util.List;
 import mate.academy.springboot.web.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class UserController {
@@ -17,6 +16,6 @@ public class UserController {
 
     @PostMapping("/users")
     public String save(@RequestBody User user) {
-        return "User created. Id: %s, email: %s, ".formatted(user.id(), user.email());
+        return "User created. Id: %s, email: %s".formatted(user.id(), user.email());
     }
 }
