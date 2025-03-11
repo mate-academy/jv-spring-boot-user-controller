1265b23e023a6c6a4c297c8d4440224efee1d587
user <graban2253@gmail.com>
2025-03-11 19:20:02 +0200

    Finished

--- src/main/java/mate/academy/springboot/web/controller/UserController.java
+++ src/main/java/mate/academy/springboot/web/controller/UserController.java
@@ -1,5 +1,23 @@
 package mate.academy.springboot.web.controller;
 
-public class UserController {
+import mate.academy.springboot.web.model.User;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @GetMapping
+    public List<User> getAll() {
+        return List.of(
+                new User(1L, "1@gmail.com"),
+                new User(2L, "2@gmail.com")
+        );
+    }
+
+    @PostMapping
+    public String save (@RequestBody User user) {
+        return "User was saved. Id: %s, name: %s".formatted(user.id(), user.email());
+    }
 }
