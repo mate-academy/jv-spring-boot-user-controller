ad5137482c326d63911399726c2089672b18b6f0
Dmitriy.Koval <dimonkoval@gmail.com>
2025-03-15 11:17:42 +0200

    removed @Configuration,scope for lombok, change hibernate.ddl-auto on validate

--- src/main/java/mate/academy/springboot/web/config/MapperConfig.java
+++ src/main/java/mate/academy/springboot/web/config/MapperConfig.java
@@ -2,9 +2,7 @@ package mate.academy.springboot.web.config;
 
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.NullValueCheckStrategy;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @org.mapstruct.MapperConfig(
         componentModel = "spring",
         injectionStrategy = InjectionStrategy.CONSTRUCTOR,
