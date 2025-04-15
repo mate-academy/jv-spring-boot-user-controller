e1f062058fea22ef9e3e94f5ddbd920fefc948bf
Olha <olha.oleshchuk@gmail.com>
2025-04-15 21:36:09 +0300

    all done - first try

--- src/main/java/mate/academy/springboot/web/model/User.java
+++ src/main/java/mate/academy/springboot/web/model/User.java
@@ -1,4 +1,8 @@
 package mate.academy.springboot.web.model;
 
 public record User(Long id, String email) {
+    public User(Long id, String email) {
+        this.id = id;
+        this.email = email;
+    }
 }
