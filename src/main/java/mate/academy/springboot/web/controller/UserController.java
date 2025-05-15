c0557bb8a46a3250d24a643fb2e86069c938f1b2
Roman Chygryn <chigrin.ra@gmail.com>
2025-05-15 20:12:42 +0200

    Controller hw

--- src/main/java/mate/academy/springboot/web/controller/UserController.java
+++ src/main/java/mate/academy/springboot/web/controller/UserController.java
@@ -1,5 +1,27 @@
 package mate.academy.springboot.web.controller;
 
+import mate.academy.springboot.web.model.User;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
+    @PostMapping
+    public String save(@RequestBody User user) {
+        return "User created. Id: %s, email: %s".formatted(user.id(), user.email());
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return List.of(
+                new User(1L, "test1@mail.com"),
+                new User(2L, "test2@mail.com")
+        );
+    }
 }
